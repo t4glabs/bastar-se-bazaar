e76eecd87f48021f6943a22b5149a7a2154aef95
meghajibhakate <meghajibhakate18@gmail.com>
2023-05-24 10:33:50 +0530

    Navbar chnaged

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-//Javascript to toggle the menu
-document.getElementById('nav-toggle').onclick = function(){
-    document.getElementById("nav-content").classList.toggle("hidden");
-}
+
+
+
+		//Javascript to toggle the menu
+		// document.getElementById('nav-toggle').onclick = function(){
+		// 	document.getElementById("nav-content").classList.toggle("hidden");
+		// }
+	
 
 
